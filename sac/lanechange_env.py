fdd1f26057f5bc5d4c29e5c0a1f986fdaf41c2d7
Janus Sucharitakul <janus.sucharitakul@gmail.com>
2026-01-02 13:14:33 -0800

    modified observation and aciton space s.t. we defined our environment

--- sac/lanechange_env.py
+++ sac/lanechange_env.py
@@ -7,10 +7,10 @@ from highway_env.envs.common.abstract import AbstractEnv
 from highway_env.envs.highway_env import HighwayEnv
 from highway_env.vehicle.controller import ControlledVehicle
 from highway_env.vehicle.kinematics import Vehicle
-
-
 from enum import Enum
 import numpy as np
+import pprint
+import wrappers
 
 # register model as gym env. Once registered, the id is usable for gym.make()
 register(
@@ -21,9 +21,9 @@ register(
 class LaneChangingEnv(HighwayEnv):
     # Action = [acceleration, steering] for now
 
-    def __init__(self, config = None):
-        super().__init__(config)
-
+    def __init__(self, config = None, render_mode = None):
+        super().__init__(config=config, render_mode=render_mode)
+     
     def _create_action_space(self):
         # accel = action[0] 
         # steer = action[1] 
@@ -38,21 +38,38 @@ class LaneChangingEnv(HighwayEnv):
         """
         Map continuous action to vehicle control.
         """
-        accel_norm, steer_norm = action
+        accel_norm, intent = float(action[0]), float(action[1])
 
         vehicle: ControlledVehicle = self.vehicle
         if vehicle is None:
             return
 
-        # ---- Longitudinal control ----
-        # Map [-1, 1] → reasonable accel range
-        max_accel = 3.0  # m/s^2. TODO: SUBJECT TO CHANGE
-        acceleration = float(np.clip(accel_norm, -1, 1)) * max_accel
+        # Longitudinal: normalized -> physical accel
+        max_accel = 3.0
+        acceleration = np.clip(accel_norm, -1.0, 1.0) * max_accel
 
-        # ---- Lateral control ----
-        # Steering mapped to lane offset / heading
-        max_steering = np.deg2rad(15)  # conservative. Subject to change
-        steering = float(np.clip(steer_norm, -1, 1)) * max_steering
+        # Lateral: decide which lane to track (current vs target)
+        deadzone = 0.2
+        current_lane_index = vehicle.lane_index  # (from_node, to_node, lane_id)
+        from_n, to_n, lane_id = current_lane_index
+
+        # determine number of lanes on the current segment (more correct than config)
+        try:
+            lanes_on_segment = len(vehicle.road.network.graph[from_n][to_n])
+        except Exception:
+            lanes_on_segment = int(self.config.get("lanes_count", 4))
+
+        # choose lane to follow
+        if abs(intent) < deadzone:
+            desired_lane_id = int(lane_id)  # keep lane
+        else:
+            desired_lane_id = int(getattr(self, "target_lane", lane_id))  # commit to goal
+
+        desired_lane_id = int(np.clip(desired_lane_id, 0, lanes_on_segment - 1))
+        target_lane_index = (from_n, to_n, desired_lane_id)
+
+        # Use controller to compute steering angle to track the chosen lane
+        steering = float(vehicle.steering_control(target_lane_index))
 
         # Apply to vehicle
         vehicle.action = {
@@ -67,22 +84,45 @@ class LaneChangingEnv(HighwayEnv):
             reward -= 1000000.0 # no crashes
 
         # encourage speed
-        reward += self.vehicle.speed / self.config["speed_limit"]
+        # reward += self.vehicle.speed / self.config["speed_limit"]
 
         # encourage lane change completion
-        if self.vehicle.lane_index[2] != self.vehicle.target_lane_index[2]:
+        # print("vehicle attributes", dir(self.vehicle))
+        # lane_index[0]: from_node, [1]: to_node, [2]: lane_id
+        if self.start_lane != self.vehicle.lane_index[2]:
             reward += 0.5
 
         # add penalties and replace with dynamic rewards later
         
         return reward
 
+    def reset(self, *args, **kwargs):
+        obs, info = super().reset(*args, **kwargs)
+        self.start_lane = self.vehicle.lane_index[2]
+        self.target_lane = self.find_target_lane(self.start_lane)
+        # limit to the lanes on the road
+        self.target_lane = int(np.clip(self.target_lane, 0, self.config.get("lanes_count", 4) - 1))
+
+        return obs, info
+    
+    def find_target_lane(self, start_lane):
+        return start_lane + 1
+        # TODO: Will need to customize this based off of the target lane really made in the trigger side right?
+
 
 def check_env(): 
     from gymnasium.utils.env_checker import check_env
     env = gym.make('lane-changing-v0', render_mode = None)
+    env = wrappers.ObsWrapper(env)
+
+
     check_env(env.unwrapped)
+    print((env.observation_space))
+    print("--Config list--")
+    pprint.pprint(env.unwrapped.config)
 
 
 if __name__ == '__main__':
     check_env()
+
+    
---
+++ sac/wrappers.py
@@ -0,0 +1,39 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+
+class ObsWrapper(gym.ObservationWrapper):
+    """
+    Flattens HighwayEnv's (vehicles_count, features) observation into 1D
+    and appends 2 goal scalars: [lane_delta_to_target, target_lane_norm].
+    """
+
+    def __init__(self, env: gym.Env):
+        super().__init__(env)
+
+        base_shape = self.observation_space.shape  # e.g. (5, 5)
+        base_dim = int(np.prod(base_shape))
+
+        self.observation_space = spaces.Box(
+            low=-np.inf,
+            high=np.inf,
+            shape=(base_dim + 2,),
+            dtype=np.float32
+        )
+
+    def observation(self, obs):
+        obs_flat = np.asarray(obs, dtype=np.float32).ravel()
+        obs_flat = np.nan_to_num(obs_flat, nan=0.0, posinf=0.0, neginf=0.0)
+
+        base_env = self.env.unwrapped
+        current_lane = int(base_env.vehicle.lane_index[2])
+        target_lane = int(getattr(base_env, "target_lane", current_lane))
+
+        lanes_count = int(base_env.config.get("lanes_count", 4))
+        denom = max(lanes_count - 1, 1)
+
+        lane_delta = float(np.clip((target_lane - current_lane) / denom, -1.0, 1.0))
+        target_norm = float(np.clip(2.0 * (target_lane / denom) - 1.0, -1.0, 1.0))
+
+        extra = np.array([lane_delta, target_norm], dtype=np.float32)
+        return np.concatenate([obs_flat, extra], axis=0).astype(np.float32, copy=False)
